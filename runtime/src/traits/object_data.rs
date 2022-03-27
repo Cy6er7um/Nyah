b66183cd4f0906d3da3a4e728d6ab26ae7fb5553
EnabledFish <hidestudio@163.com>
2022-03-27 17:27:20 +0800

    Update object_data.rs

--- runtime/src/traits/object_data.rs
+++ runtime/src/traits/object_data.rs
@@ -3,3 +3,20 @@ trait NyahObjectData: Clone {
         false
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::traits::object_data::NyahObjectData;
+
+    #[derive(Clone)]
+    struct ExampleDefaultImplement;
+
+    impl NyahObjectData for ExampleDefaultImplement {}
+
+    #[test]
+    fn test_default_implement() {
+        let instance = ExampleDefaultImplement;
+        let compare_result = instance.nyah_data_equal(&instance.clone());
+        assert_eq!(compare_result, false);
+    }
+}
