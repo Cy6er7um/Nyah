d44020873a152e680565cffc646009502509d574
EnabledFish <hidestudio@163.com>
2022-03-27 17:32:43 +0800

    Update pointer.rs

--- runtime/src/pointer/pointer.rs
+++ runtime/src/pointer/pointer.rs
@@ -3,6 +3,14 @@ pub struct NyahUtilPointer<T> {
     pub pointer: *mut T,
 }
 
+impl<T> NyahUtilPointer<T> {
+    pub fn new(pointer: *mut T) -> NyahUtilPointer<T> {
+        NyahUtilPointer {
+            pointer,
+        }
+    }
+}
+
 impl<T> Clone for NyahUtilPointer<T> {
     fn clone(&self) -> Self {
         NyahUtilPointer {
@@ -12,4 +20,22 @@ impl<T> Clone for NyahUtilPointer<T> {
     }
 }
 
+// If you use derive to automatically implement Copy,
+// then Pointer will not be able to Copy when T does not implement Copy.
 impl<T> Copy for NyahUtilPointer<T> {}
+
+#[cfg(test)]
+mod test {
+    use crate::pointer::pointer::NyahUtilPointer;
+
+    struct Example;
+
+    #[test]
+    fn test_copy_and_clone_pointer() {
+        let mut example = Example;
+        let pointer = NyahUtilPointer::new(&mut example);
+        let pointer_copy = pointer;
+        let pointer_clone = pointer.clone();
+        assert_eq!(pointer_copy.pointer, pointer_clone.pointer);
+    }
+}
